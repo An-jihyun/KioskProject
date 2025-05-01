0f7c4d2a9d6370f87389a53f00db4aa563492c72
An-jihyun <anjh322@gmail.com>
2025-05-01 13:58:33 +0900

    리드미 작성

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -11,6 +11,16 @@ public class MenuItem {
         this.description = description;
     }
 
+    //Getter
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public String getDescription() { return description; }
+
+    //Setter
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setDescription(String description) { this.description = description; }
+
     @Override
     public String toString() {
         return String.format("%s   | W %.1f | %s", name, price, description);
